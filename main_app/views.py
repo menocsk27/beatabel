2d28617d91ef57ec9a567b134f63dc6aeb5b24ba
hulkinBrain <alevoorsoorya01@gmail.com>
2019-05-17 16:47:06 +0200

    Added another font

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from . import ImageProcessing
 def index(request):
     return HttpResponse(
         "<body style=\"margin: 0\">"
-            "<h1 style=\"font-family: 'Lucida Console'; margin: 0; display: flex; height: 100vh; align-items: center; justify-content: center;\">"
+            "<h1 style=\"font-family: 'Lucida Console', 'Consolas'; margin: 0; display: flex; height: 100vh; align-items: center; justify-content: center;\">"
                 "You're very Beatable ;)"
             "</h1>"
     )
