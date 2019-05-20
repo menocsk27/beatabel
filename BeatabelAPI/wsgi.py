6f3fb8b5b1848f92c984362284816ed041d2552f
hulkinBrain <alevoorsoorya01@gmail.com>
2019-05-20 11:16:49 +0200

    Init Push

--- BeatabelAPI/wsgi.py
+++ BeatabelAPI/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BeatableAPI project.
+WSGI config for BeatabelAPI project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeatableAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BeatabelAPI.settings')
 
 application = get_wsgi_application()
