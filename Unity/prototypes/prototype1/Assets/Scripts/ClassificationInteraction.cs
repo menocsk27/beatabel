cf154d8c0e9c1f4a073c2485007c933b01d6df09
Simon Hochholzer <simon@symmetronic.com>
2019-05-21 08:25:49 +0200

    init prototype 1

---
+++ Unity/prototypes/prototype1/Assets/Scripts/ClassificationInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class ClassificationInteraction : MonoBehaviour
+{
+    public ClassificationStorage classificationStorage;
+    public GameController gameController;
+
+    private float distance;
+    private bool dragging = false;
+
+    void OnMouseDown()
+    {
+        if (Mathf.Abs(gameObject.transform.position.z) < 3.0)
+        {
+            distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            dragging = true;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (dragging)
+        {
+            // Stop dragging
+            dragging = false;
+
+            // Update score
+            gameController.AddScore(1);
+
+            // Add to stored interactions
+            float input = (gameObject.transform.position.x > 0) ? 1 : 0;
+            classificationStorage.SaveInteraction(input);
+
+            // Destroy object
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            // Allow dragging along x-y-plain
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            Vector3 prevPosition = gameObject.transform.position;
+            Vector3 position = new Vector3(rayPoint.x, rayPoint.y, prevPosition.z);
+            transform.position = position;
+        }
+    }
+}
---
+++ Unity/prototypes/prototype1/Assets/Scripts/RunnerMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RunnerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        transform.Translate(new Vector3(0, 0, -4 * Time.deltaTime), Space.World);
+    }
+}
