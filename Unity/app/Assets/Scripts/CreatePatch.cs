9c94dfaa8a675b58db8734e84b3661e7b687379b
Simon Hochholzer <simon@symmetronic.com>
2019-05-31 09:42:24 +0200

    add simple swipe interaction and score

---
+++ Unity/app/Assets/Scripts/CreatePatch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CreatePatch : MonoBehaviour
+{
+    public Transform blueprint;
+    public float patchDistance = 4;
+    public int patchCount = 100;
+
+    private void CreateImagePatch(int i)
+    {
+        Instantiate(
+            blueprint,
+            new Vector3(
+                blueprint.transform.position.x,
+                blueprint.transform.position.y,
+                (blueprint.transform.position.z + (i+1) * patchDistance)
+            ),
+            blueprint.transform.rotation
+        );
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < patchCount; i++)
+        {
+            CreateImagePatch(i);
+        }
+    }
+}
