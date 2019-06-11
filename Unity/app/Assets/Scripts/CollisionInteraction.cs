6837a78f3b4e437204b92da3be90e90eda47a8ca
M F <mihail.fomin98@gmail.com>
2019-06-11 12:29:22 +0200

    added colliders for classification

---
+++ Unity/app/Assets/Scripts/CollisionInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.name == "image")
+        //Debug.Log("Image collided!");
+    }
+
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("On trigger enter!");
+
+        //Debug.Log(other.gameObject);
+    }
+}
---
+++ Unity/app/Assets/Scripts/CreateClassifyColliders.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateClassifyColliders : MonoBehaviour
+{
+    public Transform classBlueprint;
+
+    private List<string> classes = new List<string>();
+
+    void Start()
+    {
+        classes.Add("class1");
+        classes.Add("class2");
+        classes.Add("class3");
+        classes.Add("class4");
+
+
+        int angleBetween = Mathf.FloorToInt(360 / classes.Count);
+
+        CreateColliders(angleBetween);
+    }
+
+    void CreateColliders(int angleBetween)
+    {
+        float x0 = this.gameObject.transform.position.x;
+        float y0 = this.gameObject.transform.position.y;
+        float radius = this.gameObject.transform.localScale.x / 2;
+
+        float angle = 0 * Mathf.PI;
+
+       
+
+        for (int i = 0; i < classes.Count; i++)
+        {
+
+            Vector3 newLocation = new Vector3(
+                   x0 + Mathf.Cos(angle) * radius,
+                   y0 + Mathf.Sin(angle) * radius,
+                   classBlueprint.transform.position.z
+               );
+
+
+            Debug.Log(classes[i]);
+            Debug.Log("angle (rad): " + angle);
+            Debug.Log("new loc: " + newLocation + "\n");
+
+            var myClass = Instantiate(
+                classBlueprint,
+                newLocation,
+                classBlueprint.transform.rotation
+            );
+
+            myClass.transform.parent = this.gameObject.transform;
+
+            angle = angle + Mathf.Deg2Rad * angleBetween;
+        }
+    }
+
+    private void Update()
+    {
+        this.gameObject.transform.Rotate(0f, 0f, 1f);
+    }
+
+}
