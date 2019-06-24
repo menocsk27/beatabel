ba460483e827449c98e03ded074f2c5f5fbdedba
hulkinBrain <alevoorsoorya01@gmail.com>
2019-06-24 21:48:59 +0200

    Changed few lines in settings.py

--- BeatabelAPI/settings.py
+++ BeatabelAPI/settings.py
@@ -96,12 +96,8 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-
-
-db_from_env = dj_database_url.config()
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES['default'] = dj_database_url.config()
 
