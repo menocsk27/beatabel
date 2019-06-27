8880d920508c99c1e423f0c6417ff1e269615c0a
hulkinBrain <alevoorsoorya01@gmail.com>
2019-06-27 09:59:15 +0200

    Added librosa in ProcessSong

--- main_app/ProcessSong.py
+++ main_app/ProcessSong.py
@@ -1,6 +1,16 @@
-import aubio
+import librosa
 import numpy as np
 
-def test(source):
-    src = aubio.source(source)
-    return(src.uri, src.duration)
+def test1(source):
+    input_file = source
+    y, sr = librosa.load(input_file)
+
+    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+    print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+    # 4. Convert the frame indices of beat events into timestamps
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+    print(beat_times)
+    print()
+
+test1("1.mp3")
