133162677c91ef83c996f444fe2d4a4ee4db4564
hulkinBrain <alevoorsoorya01@gmail.com>
2019-08-09 23:29:24 +0200

    Debug to False

--- BeatabelAPI/settings.py
+++ BeatabelAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@4(1ij3ynw36%-bq)$foztv0118y^nq+r)arf#h93rk8a+%l#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "beatabelapi.herokuapp.com"]
 
